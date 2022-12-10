f15946e36763bad7efa463073b67652ce39fa26d
Etherrreal <65111206+lifeafter619@users.noreply.github.com>
2022-12-10 16:35:46 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'db87e7627dd44e6fb4d470f8ecf73391',
+  rootNotionPageId: '7877d045c1654733ad9cb22dbd9ef14a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  name: '619的主页',
+  domain: 'www.66619.eu.org',
   author: '619',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Just a cloud.',
 
   // social usernames (optional)
   github: 'lifeafter619',
